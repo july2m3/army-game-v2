a9e0a04219e852099904b078f3fc98ef10f7f48b
july2m3 <july2m3@gmail.com>
2020-07-15 17:38:23 -0500

    Updates

---
+++ precache-manifest.afc4ba4db52780ecc0d6953f5b71637f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6a1b6e456c608f08be13b07cf556ba3",
+    "url": "/army-game-v2/index.html"
+  },
+  {
+    "revision": "f3e1d20702df0f0d6487",
+    "url": "/army-game-v2/static/css/main.31605f37.chunk.css"
+  },
+  {
+    "revision": "03f8c5db4a0c07c8806f",
+    "url": "/army-game-v2/static/js/2.be1541ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/army-game-v2/static/js/2.be1541ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3e1d20702df0f0d6487",
+    "url": "/army-game-v2/static/js/main.4f67ca4f.chunk.js"
+  },
+  {
+    "revision": "b3710bac19e821c7503c",
+    "url": "/army-game-v2/static/js/runtime-main.03ae2385.js"
+  },
+  {
+    "revision": "b3f0e1f543e6cbe9e2b7f7654fa65ee7",
+    "url": "/army-game-v2/static/media/borderless.b3f0e1f5.png"
+  }
+]);
